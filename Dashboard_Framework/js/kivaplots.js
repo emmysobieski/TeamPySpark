227a26a1071555b0415514b509852b6370c9defe
SheriKarasick <61953736+SheriKarasick@users.noreply.github.com>
2020-08-09 21:46:41 -0400

    Add files via upload

--- Dashboard_Framework/js/kivaplots.js
+++ Dashboard_Framework/js/kivaplots.js
@@ -3,10 +3,9 @@
 
 function init() {
     var selector = d3.select("#selDataset");
-    d3.json("dashboard_data.json").then((data) => {
+    d3.json("formatted_dropdown_dashboard.json").then((data) => {
       console.log(data);
       var array = Object.keys(data.COUNTRY_NAME);
-      //var sampleNames = data.COUNTRY_NAME;
       array.forEach(function(sample) { console.log(sample); 
         selector
           .append("option")
@@ -16,34 +15,31 @@ function init() {
   });
 }
   
-init();
+  init();
 
   function optionChanged(newSample) {
     console.log(newSample);
     buildMetadata(newSample);
     //buildCharts(newSample);
-    //buildCharts2(newSample);
+    buildCharts2(newSample);
   }
 
   function pageLoad() {
-    //get_country = result.COUNTRY_NAME[0]
-    //var country = COUNTRY_NAME[0]
-    d3.json("dashboard_data.json").then((data) => {
+    d3.json("formatted_dropdown_dashboard.json").then((data) => {
     d3.select(window).on("load", data.sample);
     buildMetadata(0);
     //buildCharts("Afghanistan");
-    //buildCharts2("Afghanistan");
+    buildCharts2(0);
   });
 }
 window.onload = pageLoad();
 
-
   function buildMetadata(sample) {
-    d3.json("dashboard_data.json").then((data) => {
+    d3.json("formatted_dropdown_dashboard.json").then((data) => {
       var PANEL = d3.select("#sample-metadata");
   
       PANEL.html("");
-      PANEL.append("h6").text(["Total Loans: " + data.total_loan[sample]]);
+      PANEL.append("h6").text(["Total Loans: " + data.total_loan_amount[sample]]);
       PANEL.append("h6").text(["Minimum Individual Loan: " + data.min_loan[sample]]);
       PANEL.append("h6").text(["Maximum Individual Loan: " + data.max_loan[sample]]);
       PANEL.append("h6").text(["Country Population(2017): " + data.population_in_thousands_2017[sample]]);
@@ -53,81 +49,25 @@ window.onload = pageLoad();
       PANEL.append("h6").text(["Fertility Rate: " + data.fertility_rate_total_live_births_per_woman[sample]]);
     });
   }
-
-
-//function for accessing data and making charts
-    function buildCharts(sample) {
-        d3.json("dashboard_data.json").then((data) => {
-        console.log("hello");
-        var metadata = data.metadata;
-        var samples = data.samples;
-        var resultsArray = samples.filter(sample_object => sample_object.COUNTRY_NAME == sample);
-        var result = resultsArray[0];
-        console.log(result);
-
-        var sample_values = result.sample_values;
-        var num_bacteria = result.sample_values;
-        var bacteria_id = result.otu_ids;   
-        var bacteria_names = result.otu_labels;  
-        console.log(num_bacteria);
-        console.log(bacteria_id);
-        
-        //build a bar chart
-        var y_data = bacteria_id.slice(0,10).map(name => "OTU "+name.toString()).reverse();
-        var x_data = num_bacteria.slice(0,10);
-        console.log(x_data)
-        console.log(y_data)
-
-        var chartData = [
-        {x: x_data,
-         y: y_data,
-         type: "bar",
-         orientation: "h"
-        }];
-    
-        var chartLayout = {
-            title: "Most Frequently Occuring Bacteria<br /> by ID Number",
-            xaxis: {title: "Number of Individual Bacteria in Sample"},
-            yaxis: {title: "Bacteria ID"}
-        };
-        
-        Plotly.newPlot("bar", chartData, chartLayout);
-        
-    });
-}
     // creating a bubble chart
     function buildCharts2(sample) {
-        d3.json("dashboard_data.json").then((data) => {
+        d3.json("formatted_dropdown_dashboard.json").then((data) => {
                 console.log("hello");
-            //var metadata = data.metadata;
-            var samples = data.samples;
-            var resultsArray = samples.filter(sample_object => sample_object.id == sample);
-            var result = resultsArray[0];
-            console.log(result);
     
-            var sample_values = result.sample_values;
-            var num_bacteria2 = result.sample_values;
-            var bacteria_id2 = result.otu_ids;   
-            var bacteria_names2 = result.otu_labels;  
-            console.log(num_bacteria2);
-            console.log(bacteria_id2);
-            console.log(bacteria_names2);
-                
-            //build a bar chart
-            //var bubble_y_data = num_bacteria2;
-            //var bubble_x_data = bacteria_id2;
-            //console.log(bubble_x_data)
-            //console.log(bubble_y_data)
+            var total_loan_number = data.total_loans_per_country[sample];
+            var time_to_fund = data.avg_funding_time[sample];
 
+            console.log(total_loan_number);
+            console.log(time_to_fund);     
 
             var trace1 = {
-                x: bacteria_id2,
-                y: num_bacteria2,
-                text: bacteria_names2,
+                x: total_loan_number,
+                y: time_to_fund,
+                text: total_loan_number,
                 mode: "markers",
                     marker: {
-                        color: bacteria_id2,
-                        size: num_bacteria2,
+                        color: time_to_fund,
+                        size: total_loan_number,
                         colorScale: "Electric",
                         type: "heatmap"
                     }};
@@ -135,9 +75,9 @@ window.onload = pageLoad();
             var data2 = [trace1];
             
             var layout = {
-                title: 'Belly Button Bacteria Counts',
-                xaxis: {title: "Specie Identification Number of Bacteria"},
-                yaxis: {title: "Number of Individual Bacteria in Sample"},
+                title: 'Kiva Loan Time vs. Number of Loans by Country',
+                xaxis: {title: "Number of Loans Total for Country"},
+                yaxis: {title: "Number of Days until Funding Success!"},
                 //showlegend: true,
                 //height: 600,
                 //width: 600
